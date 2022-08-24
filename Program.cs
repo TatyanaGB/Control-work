3869bd4027b8ded63105df9529160de62985e131
Tatyana <TatyanaIvanova@example.com>
2022-08-24 22:43:16 +0300

    Add decision

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] myArray = new string[] { "hello", "2", "world",  ":-)" };
+string[] myTwoArray = new string[myArray.Length];
+CharsArray(myArray, myTwoArray);
+PrintArray(myTwoArray);
+
+void CharsArray(string[] myArray, string[] myTwoArray)
+ {
+    int count = 0;
+    for (int i = 0; i < myArray.Length; i++)
+    {
+      if (myArray[i].Length <= 3)
+      {
+        myTwoArray[count] = myArray[i];
+        count++;
+      }  
+    }   
+  }    
+
+void PrintArray(string[] myArray)
+{
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write($"{myArray[i]} ");
+    }
+    Console.WriteLine();
+}
